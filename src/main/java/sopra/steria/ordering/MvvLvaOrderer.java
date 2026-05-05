37664fe5b03aa32807b163663c27be3ff2a70a5e
patrickKottman <patkottman@gmail.com>
2026-05-05 11:32:48 +0200

    init helpers, mvvlva

--- src/main/java/sopra/steria/ordering/MvvLvaOrderer.java
+++ src/main/java/sopra/steria/ordering/MvvLvaOrderer.java
@@ -2,38 +2,52 @@ package sopra.steria.ordering;
 
 import knight.clubbing.core.BBoard;
 import knight.clubbing.core.BMove;
-import sopra.steria.evaluation.Evaluator;
+import knight.clubbing.core.BPiece;
 
-import java.util.HashMap;
-import java.util.Map;
+import static sopra.steria.helpers.Helpers.pieceValue;
+import static sopra.steria.helpers.Helpers.scoreCapture;
 
 public class MvvLvaOrderer implements MoveOrderer {
 
-    public MvvLvaOrderer() {
-        Map<String, Integer> pieceValues = new HashMap<String, Integer>();
-        pieceValues.put("P", 100);
-        pieceValues.put("N", 350);
-        pieceValues.put("B", 350);
-        pieceValues.put("R", 525);
-        pieceValues.put("Q", 1000);
-    }
-
     @Override
     public void orderMoves(BMove[] moves, BBoard board) {
+        int[] scores = new int[moves.length];
 
         for (int i = 0; i < moves.length; i++) {
-
+            scores[i] = score(moves[i], board);
         }
+
+        sortMovesByScore(moves, scores);
     }
 
-    /**
-     *
-     * @param victimValue captured piece
-     * @param attackerValue attacking piece
-     * Score is weighted based on that victim value is more important
-     * @return move score
-     */
-    private int scoreCapture(int victimValue, int attackerValue) {
-        return victimValue * 10 - attackerValue; // Higher score for capturing more valuable pieces
+    private int score(BMove move, BBoard board) {
+        int[] pieceBoards = board.getPieceBoards();
+        int attacker = BPiece.getPieceType(pieceBoards[move.startSquare()]);
+        int victim = BPiece.getPieceType(pieceBoards[move.targetSquare()]);
+        int attackerVal = pieceValue(attacker);
+        int victimVal = pieceValue(victim);
+
+        if (victim == BPiece.none) {
+            return 0; // don't penalize quiet moves
+        }
+        return scoreCapture(victimVal, attackerVal);
+    }
+
+    private void sortMovesByScore(BMove[] moves, int[] scores) {
+        for (int i = 0; i < moves.length - 1; i++) {
+            for (int j = i + 1; j < moves.length; j++) {
+                if (scores[j] > scores[i]) {
+                    // Swap moves
+                    BMove tempMove = moves[i];
+                    moves[i] = moves[j];
+                    moves[j] = tempMove;
+
+                    // Swap scores
+                    int tempScore = scores[i];
+                    scores[i] = scores[j];
+                    scores[j] = tempScore;
+                }
+            }
+        }
     }
 }
