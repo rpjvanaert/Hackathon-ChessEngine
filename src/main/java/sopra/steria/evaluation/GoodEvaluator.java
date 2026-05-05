b8b2bb65aa4371f3802f55d788f506dbcb93a222
Lars.Villevoye <lars.villevoye@ka.prorail.nl>
2026-05-05 14:44:34 +0200

    Merged piece values

--- src/main/java/sopra/steria/evaluation/GoodEvaluator.java
+++ src/main/java/sopra/steria/evaluation/GoodEvaluator.java
@@ -5,21 +5,14 @@ import knight.clubbing.core.BPiece;
 import sopra.steria.helpers.Helpers;
 
 public class GoodEvaluator implements Evaluator {
-    private static final int PAWN_VALUE   = 100;
-    private static final int KNIGHT_VALUE = 320;
-    private static final int BISHOP_VALUE = 330;
-    private static final int ROOK_VALUE   = 500;
-    private static final int QUEEN_VALUE  = 900;
-    private static final int KING_VALUE   = 20000;
-
     @Override
     public int evaluate(BBoard board) {
         int score = 0;
         int[] pieces = board.getPieceBoards(); // length 64, one entry per square
 
-        for (int sq= 0; sq < 64; sq++) {
+        for (int sq = 0; sq < 64; sq++) {
             int piece = pieces[sq];
-            if(piece == BPiece.none) continue;
+            if (piece == BPiece.none) continue;
 
             boolean isWhite = BPiece.isWhite(piece);
             int type = BPiece.getPieceType(piece);
@@ -29,10 +22,10 @@ public class GoodEvaluator implements Evaluator {
             // Black squares are used as-is.
             int mirroredIndex = isWhite ? (sq ^ 56) : sq;
 
-            score += isWhite
+            score = (isWhite
                     ? PstTables.PST[type][mirroredIndex]
-                    : -PstTables.PST[type][mirroredIndex]; // PST
-            score += Helpers.pieceValue(type); // Material Value
+                    : -PstTables.PST[type][mirroredIndex]) // PST
+                    * Helpers.pieceValue(type); // Material Value
         }
 
         return board.isWhiteToMove() ? score : -score;
